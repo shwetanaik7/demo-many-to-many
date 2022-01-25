7e4ead7050cfa8431e617cab18215eea7c00441c
shwetanaik7 <shwetabootcamp@gmail.com>
2022-01-25 06:41:55 -0500

    first commit

---
+++ src/main/java/com/example/demo/DemoManyToManyApplication.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.repository.TagRepository;
+import com.example.demo.model.Post;
+import com.example.demo.model.Tag;
+
+@SpringBootApplication
+public class DemoManyToManyApplication implements CommandLineRunner{
+	
+	@Autowired
+	private TagRepository tagRepository;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoManyToManyApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// Cleanup the tables
+		postRepository.deleteAllInBatch();
+		tagRepository.deleteAllInBatch();
+
+		// =======================================
+
+		// Create a Post
+		Post post = new Post("Hibernate Many to Many Example with Spring Boot",
+				"Learn how to map a many to many relationship using hibernate",
+				"Entire Post content with Sample code");
+
+		// Create two tags
+		Tag tag1 = new Tag("Spring Boot");
+		Tag tag2 = new Tag("Hibernate");
+
+
+		// Add tag references in the post
+		post.getTags().add(tag1);
+		post.getTags().add(tag2);
+
+		// Add post reference in the tags
+		tag1.getPosts().add(post);
+		tag2.getPosts().add(post);
+
+		postRepository.save(post);
+
+		// =======================================
+
+	}
+}
